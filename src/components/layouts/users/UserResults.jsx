cead31ab4c7d1b2ab786d4c5c1b2190cdfb08fd5
aniebodavid <aniebovictor001@gmail.com>
2022-07-25 20:12:14 -0700

    add github Api

---
+++ src/components/layouts/users/UserResults.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react"
+
+function UserResults() {
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
+  const fetchUsers = async () => {
+    const response = await fetch(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
+    const data = await response.json()
+    setUsers(data)
+    setLoading(false)
+  }
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    )
+  } else {
+    return <h3>Loading......</h3>
+  }
+}
+
+export default UserResults
